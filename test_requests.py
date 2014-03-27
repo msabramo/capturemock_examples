e49ef854e05e79c4b3d5c5f3b247e917be7df5e3
Marc Abramowitz <marc@marc-abramowitz.com>
2014-03-27 14:46:16 -0700

    test_requests.py: Make into a real py.test test

--- test_requests.py
+++ test_requests.py
@@ -1,15 +1,15 @@
 from capturemock import capturemock, RECORD, REPLAY
 
-@capturemock("requests")
-def requests_get():
+
+@capturemock("requests", mode=REPLAY)
+def test_requests_get():
     import requests
 
     resp = requests.get("http://www.google.com/")
-    print("resp = %r" % resp)
-    print("resp.url = %r" % resp.url)
-    print("resp.status_code = %r" % resp.status_code)
-    print("resp.text = %r" % resp.text)
-    print("resp.headers = %r" % resp.headers)
-    print("resp.headers['Content-Type'] = %r" % resp.headers['Content-Type'])
-
-requests_get()
+    assert isinstance(resp, requests.Response)
+    assert repr(resp) == '<Response [999]>'
+    assert resp.url == u'http://www.google.com/'
+    assert resp.status_code == 999
+    assert resp.text == u'This is a fake response from CaptureMock!'
+    assert len(repr(resp.headers)) > 0
+    assert resp.headers['Content-Type'] == 'text/plain; charset=UTF-8'
