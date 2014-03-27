58edb475dd22766aba70f0b24f02798924e619c2
Marc Abramowitz <marc@marc-abramowitz.com>
2014-03-27 14:46:49 -0700

    Add tox.ini and setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='capturemock_examples',
+    version="trunk",
+    url="https://github.com/msabramo/capturemock_examples",
+    author="Marc Abramowitz",
+    author_email="msabramo@gmail.com",
+)
