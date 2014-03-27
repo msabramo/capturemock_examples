067e3a423a725a61e31b10b9cd0623d8379c3128
Marc Abramowitz <marc@marc-abramowitz.com>
2014-03-27 16:19:52 -0700

    Make it so one can run `python test_request.py` from shell
    and see output show in README.md

--- test_requests.py
+++ test_requests.py
@@ -13,3 +13,20 @@ def test_requests_get():
     assert resp.text == u'This is a fake response from CaptureMock!'
     assert len(repr(resp.headers)) > 0
     assert resp.headers['Content-Type'] == 'text/plain; charset=UTF-8'
+
+
+@capturemock("requests", mode=REPLAY)
+def requests_get_interactive():
+    import requests
+
+    resp = requests.get("http://www.google.com/")
+    print("resp = %r" % resp)
+    print("resp.url = %r" % resp.url)
+    print("resp.status_code = %r" % resp.status_code)
+    print("resp.text = %r" % resp.text)
+    print("resp.headers = %r" % resp.headers)
+    print("resp.headers['Content-Type'] = %r" % resp.headers['Content-Type'])
+
+
+if __name__ == '__main__':
+    requests_get_interactive()
